be1293b847ee853778c737fbd860eea31adaafe3
monkamsteve <janesteve237@gmail.com>
2024-06-14 02:35:39 -0400

    buiding a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Grid, GridItem, Show} from "@chakra-ui/react";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+       <Grid templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"`}} >
+           <GridItem pl='2' bg='orange.300' area={'nav'}>
+               Nav
+           </GridItem>
+         <Show above='lg'>
+             <GridItem pl='2' bg='pink.300' area={'aside'}> Aside </GridItem>
+         </Show>
+           <GridItem pl='2' bg='blue.300' area={'main'}> Main </GridItem>
+       </Grid>
+
+   </>
   );
 }
 
